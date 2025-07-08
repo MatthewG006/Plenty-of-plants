9e172941ba778df62d8bd5756b5a805c0f246e1f
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-07-08 19:05:44 +0000

    use san serifs for all fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Alegreya&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         {children}
