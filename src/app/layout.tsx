4f4441d28fb4eeb8400964361db2609fda7ab58e
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-07-09 03:09:37 +0000

    How can I export this for android

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#4FDF81" />
       </head>
       <body className="font-body antialiased">
         {children}
