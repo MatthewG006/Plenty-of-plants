15d78433952f28fb124d23ef1f6151c159aa30e3
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-07-13 19:33:42 +0000

    play the music even on the login screen

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,5 @@
 
 import BottomNavBar from '@/components/bottom-nav-bar';
-import { AudioProvider } from '@/context/AudioContext';
-import MusicPlayer from '@/components/music-player';
 
 export default function AppLayout({
   children,
@@ -9,12 +7,9 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <AudioProvider>
-      <div className="min-h-screen w-full">
-        <main className="pb-24">{children}</main>
-        <BottomNavBar />
-        <MusicPlayer />
-      </div>
-    </AudioProvider>
+    <div className="min-h-screen w-full">
+      <main className="pb-24">{children}</main>
+      <BottomNavBar />
+    </div>
   );
 }
