4791e5720d0bce2b7e326dc9589e6864867ebfc6
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-07-18 20:24:01 +0000

    Check the file where the key is located

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,13 +1,11 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
-/*
 if (!process.env.GOOGLE_API_KEY) {
   throw new Error(
     'GOOGLE_API_KEY environment variable not set. Please provide your API key in the .env file.'
   );
 }
-*/
 
 export const ai = genkit({
   plugins: [
