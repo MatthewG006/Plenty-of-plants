ee28cf919e6ef8b06b196fab760ca80734098e7c
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-07-19 05:08:18 +0000

    Users are having issues where they tap the navigation buttons at the bot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AudioProvider } from '@/context/AudioContext';
 import MusicPlayer from '@/components/music-player';
-import { AuthProvider } from '@/context/AuthContext';
 
 export const metadata: Metadata = {
   title: 'Plenty of Plants',
@@ -20,13 +19,11 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head />
       <body className="font-body antialiased">
-        <AuthProvider>
-          <AudioProvider>
-            {children}
-            <Toaster />
-            <MusicPlayer />
-          </AudioProvider>
-        </AuthProvider>
+        <AudioProvider>
+          {children}
+          <Toaster />
+          <MusicPlayer />
+        </AudioProvider>
       </body>
     </html>
   );
