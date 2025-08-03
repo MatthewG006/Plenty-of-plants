9d43dc038ecdd36a866ef38b95702e069cc830d9
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-08-03 18:55:16 +0000

    Reduce the padding between each row

--- src/app/(app)/garden/page.tsx
+++ src/app/(app)/garden/page.tsx
@@ -201,7 +201,7 @@ export default function GardenPage() {
         <div className="relative z-10 p-4 space-y-4">
             <section>
               {allPlants.length > 0 ? (
-                <div className="grid grid-cols-3 gap-y-6 gap-x-8">
+                <div className="grid grid-cols-3 gap-y-4 gap-x-8">
                   {allPlants.map((plant) => (
                     <PlantCard
                       key={plant.id}
