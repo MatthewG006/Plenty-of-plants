1e3e062ce230dc1c871ededce3a4aceec8a4d858
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-08-03 19:58:54 +0000

    Make the height class correspond to 95 pixels

--- src/app/(app)/garden/page.tsx
+++ src/app/(app)/garden/page.tsx
@@ -33,7 +33,7 @@ function PlantCard({ plant, onSelectPlant, processedImage, className }: { plant:
             className={cn("group w-full relative cursor-pointer hover:scale-105 transition-transform", className)}
             onClick={() => onSelectPlant(plant)}
         >
-            <div className="relative flex items-center justify-center mb-2 h-28">
+            <div className="relative flex items-center justify-center mb-2 h-24">
                 {imageToDisplay !== 'placeholder' ? (
                     <div className="h-full w-full relative">
                         <Image src={imageToDisplay} alt={plant.name} fill sizes="100px" className="object-contain" data-ai-hint={plant.hint} />
