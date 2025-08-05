effbfa19544c497b440c81648305a0ab7c111d32
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-08-05 02:56:58 +0000

    increase the top position of the first two rows by 10

--- src/app/(app)/garden/page.tsx
+++ src/app/(app)/garden/page.tsx
@@ -25,9 +25,9 @@ const SECOND_EVOLUTION_LEVEL = 25;
 
 const plantPositions = [
     // Row 1
-    { top: '10%', left: '15%' }, { top: '10%', left: '50%' }, { top: '10%', left: '85%' },
+    { top: '20%', left: '15%' }, { top: '20%', left: '50%' }, { top: '20%', left: '85%' },
     // Row 2
-    { top: '45%', left: '15%' }, { top: '45%', left: '50%' }, { top: '45%', left: '85%' },
+    { top: '55%', left: '15%' }, { top: '55%', left: '50%' }, { top: '55%', left: '85%' },
     // Row 3
     { top: '84%', left: '15%' }, { top: '84%', left: '50%' }, { top: '84%', left: '85%' },
     // Row 4
