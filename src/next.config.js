ca97dab09dab05669b035d6d5fb57862a58d36ae
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-10-29 03:58:30 +0000

    ## AI
    ### Analyzing network data
    
    **Request:**
    ````
    Request URL: https:

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -30,5 +31,5 @@ const nextConfig = {
     NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   },
 };
-
+// Triggering a rebuild to fix module loading error
 module.exports = nextConfig;
