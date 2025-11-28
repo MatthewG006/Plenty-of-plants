6d5d1970a23f14b3574b12b7760aa56c497b8aaa
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-11-28 06:06:21 +0000

    Settings and profile missing from home page

--- src/components/home/HomeContent.tsx
+++ src/components/home/HomeContent.tsx
@@ -1,19 +1,34 @@
 
+'use client';
+
 import ChallengeList from './ChallengeList';
 import Draws from './Draws';
 import LatestPlant from './LatestPlant';
 import GameTips from './GameTips';
 import { Separator } from '@/components/ui/separator';
+import { Button } from '@/components/ui/button';
+import { User, Settings } from 'lucide-react';
+import Link from 'next/link';
 
 export default function HomeContent() {
 
   return (
     <>
       <div className="p-4 space-y-6 bg-white pb-20">
-        <header className="flex flex-col items-center space-y-2">
+        <header className="flex items-center justify-between">
+          <div className="w-10">
+            <Button asChild variant="ghost" size="icon">
+              <Link href="/profile"><User /></Link>
+            </Button>
+          </div>
           <h1 className="text-3xl text-primary font-bold text-center">
             Plenty Of Plants
           </h1>
+          <div className="w-10">
+             <Button asChild variant="ghost" size="icon">
+              <Link href="/settings"><Settings /></Link>
+            </Button>
+          </div>
         </header>
 
         <main className="space-y-6">
