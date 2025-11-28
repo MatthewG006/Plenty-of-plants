64ea8fa50bbd83babcd05c4c5afb0b008254620a
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-11-28 16:44:20 +0000

    Didn't work to solve the UI issue

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthGuard>
       <div className="relative flex flex-col h-full bg-background">
-        <main className="flex-1 overflow-y-auto">{children}</main>
+        <main className="flex-1 overflow-y-auto pb-16">{children}</main>
         <BottomNavBar />
       </div>
     </AuthGuard>
