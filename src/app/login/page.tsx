ba1f1b66801095d39e4f6307196e5c7e658634ec
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-11-29 05:45:16 +0000

    make the background https://firebasestorage.googleapis.com/v0/b/plentyof

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+
 import PublicRoute from "@/components/auth/PublicRoute";
 import LoginForm from "./LoginForm";
 
@@ -6,7 +7,7 @@ export default function LoginPage() {
     <PublicRoute>
       <main 
         className="flex min-h-screen items-center justify-center bg-cover bg-center p-4"
-        style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/plentyofplants-108e8.appspot.com/o/splash-bg.png?alt=media&token=ba86f593-e391-48c8-99cc-babbfb594b1b')" }}
+        style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/plentyofplants-108e8.firebasestorage.app/o/splash-bg.png?alt=media&token=ba86f593-e391-48c8-99cc-babbfb594b1b')" }}
       >
         <LoginForm />
       </main>
