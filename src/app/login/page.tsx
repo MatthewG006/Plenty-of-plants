e51d1e51f8950a95dbf5014bc242a9029b8a27cf
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2025-11-29 02:04:27 +0000

    background-image: linear-gradient(to top, rgba(83, 223, 129, 0.8), rgba(

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,16 @@ import LoginForm from "./LoginForm";
 export default function LoginPage() {
   return (
     <PublicRoute>
-      <LoginForm />
+      <main
+        className="flex min-h-screen items-center justify-center p-4"
+        style={{
+          backgroundImage: "linear-gradient(to top, rgba(83, 223, 129, 0.8), rgba(83, 223, 129, 0.05)), url('https://firebasestorage.googleapis.com/v0/b/plentyofplants-108e8.appspot.com/o/splash-bg.png?alt=media&token=ba86f593-e391-48c8-99cc-babbfb594b1b')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <LoginForm />
+      </main>
     </PublicRoute>
   );
 }
