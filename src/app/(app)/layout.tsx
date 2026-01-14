d9097b1087ecd1446ac618ccbb70fe4b19acdc2c
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2026-01-14 19:23:18 +0000

    I have to bypass the login screen for a crawler. The lack of public acce

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -6,11 +6,11 @@ import AuthGuard from '@/components/auth/AuthGuard';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthGuard>
+    // <AuthGuard>
       <div className="relative flex flex-col h-full bg-background">
         <main className="flex-1 overflow-y-auto pb-16">{children}</main>
         <BottomNavBar />
       </div>
-    </AuthGuard>
+    // </AuthGuard>
   );
 }
