4f2885fe26076e2749f83a0b53ff68c8dda3397a
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2026-01-17 22:33:05 +0000

    non logged in users should see the home page

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,15 +2,12 @@
 'use client';
 
 import BottomNavBar from '@/components/bottom-nav-bar';
-// import AuthGuard from '@/components/auth/AuthGuard';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    // <AuthGuard>
       <div className="relative flex flex-col h-full bg-background">
         <main className="flex-1 overflow-y-auto pb-16">{children}</main>
         <BottomNavBar />
       </div>
-    // </AuthGuard>
   );
 }
