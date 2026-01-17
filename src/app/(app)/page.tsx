69cf78b2304714c879ff4d44b12653e5c2451eec
Matthew Gonzalez <matthew.gonzalez006@gmail.com>
2026-01-17 22:34:56 +0000

    Try fixing this error: `Runtime TypeError: Failed to execute 'measure' o

--- src/app/(app)/page.tsx
+++
@@ -1,9 +0,0 @@
-
-'use client';
-
-import HomePage from "./home/page";
-
-// This page now simply renders the home page, as the layout handles auth protection.
-export default function AppRootPage() {
-  return <HomePage />;
-}
